26b0902b792d8366d8ec80582532d126a0cd6be8
rodrigotorrico <rodrigo.torricodelcastillo@hotmail.com>
2019-02-12 09:33:25 -0400

    green test for Buzz

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,6 +1,10 @@
 const fizzbuzz = require('./fizzBuzz.js')
 
 test('returns Fizz for multiple of 3 ', () => {
-    expect(fizzbuzz(3)).toBe('Fizz');
+    expect(fizzbuzz(6)).toBe('Fizz');
+});
+
+test('returns Buzz for multiple of 5 ', () => {
+    expect(fizzbuzz(5)).toBe('Buzz');
 });
 

507899c99474769eeaf53b99a4f34df206b8c20e
rodrigotorrico <rodrigo.torricodelcastillo@hotmail.com>
2019-02-12 09:45:01 -0400

    refactoring: removed if statements with switch

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,23 +1,15 @@
 function fizzbuzz (i) {
-
-        if (i % 3 === 0 && i % 5 === 0)
-        {
-          return"FizzBuzz";                    
-        }
-        else 
-          if(i % 3 === 0)
-          {
-            return"Fizz";
-          }
-          else 
-            if(i % 5 === 0)
-            {
-              return"Buzz";
-            }
-            else
-              returni;
-    
+  switch(true) {
+    case ( i % 3 === 0 && i % 5 === 0 ):
+      return 'FizzBuzz';
+    case i % 3 === 0:
+      return 'Fizz';
+    case i % 5 === 0:
+      return 'Buzz';
+    default:
+      return i;
   }
+}
 
   for(i = 0; i < 100; i++) 
   {
